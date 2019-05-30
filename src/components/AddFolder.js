a396b648a953f831e88bcd4bbe3248b50c6c146c
nbeers22 <nbeers22@gmail.com>
2019-05-30 12:06:35 -0400

    added form validation onChange to add folder button

--- src/components/AddFolder.js
+++ src/components/AddFolder.js
@@ -6,18 +6,38 @@ export default class AddFolder extends Component {
   constructor(){
     super();
     this.nameInput = React.createRef();
+    this.state = {
+      errorMsg: '',
+      formValid: false
+    }
   }
 
   static contextType = FolderNoteContext;
 
+  validateName = (value) => {
+    if (value.length === 0) {
+      this.setState({
+        errorMsg: 'Please enter a folder name',
+        formValid: false
+      });
+    }else if (value.length > 0 && value.length < 3) {
+      this.setState({
+        errorMsg: 'Folder name must be at least 3 characters',
+        formValid: false
+      });
+    }else {
+      this.setState({
+        errorMsg: '',
+        formValid: true
+      });
+    }
+  }
+
   newFolder = (event) => {
     event.preventDefault();
     const { value } = this.nameInput.current;
-    if(value.length > 0){
-      document.querySelector('#error').innerText = "";
+    if(this.state.formValid){
       this.context.contextValue.addFolder(value);
-    }else{
-      document.querySelector('#error').innerText = "Please enter a folder name";
     }
   }
 
@@ -27,9 +47,9 @@ export default class AddFolder extends Component {
         <h1>Add New Folder</h1>
         <form onSubmit={event => this.newFolder(event)} className="add-folder-form">
           <label htmlFor="folder-name">Folder Name</label>
-          <input type="text" ref={this.nameInput} id="folder-name"/>
-          <div id="error"></div>
-          <button type="submit">Submit</button>
+          <input type="text" ref={this.nameInput} defaultValue="" onChange={e => this.validateName(e.target.value)} id="folder-name"/>
+          <div id="error">{this.state.errorMsg}</div>
+          <button disabled={!this.state.formValid} type="submit">Submit</button>
         </form>
       </div>
     )
